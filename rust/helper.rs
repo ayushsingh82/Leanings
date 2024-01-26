8fcde4e07c3f0c84a742bdc7e82c7951465bf2b3
ayushsingh82 <ayushsinghmi711@gmail.com>
2024-01-26 12:07:53 +0530

    rust code

---
+++ rust/helper.rs
@@ -0,0 +1,7 @@
+pub mod namehelpers { 
+pub fn get_full_name(first:&str,last:&str)->String{
+    let full_name=format!("{0} {1}", first , last);
+    return full_name;
+}
+}
+
---
+++ rust/main.rs
@@ -0,0 +1,47 @@
+pub mod helper;
+
+fn main(){
+    println!("hello");
+    // test_func();
+    let myresult:String=helper::namehelpers::get_full_name("Ayush","SINGH");
+    println!("Hello from {0}",myresult);
+
+}
+
+
+
+#[allow(dead_code)]
+fn test_func(){
+    let x:f32=255.0;
+    let y:i32= x as i32 -5;
+    println!("{}",y);
+
+
+    let mut iam:bool=true;
+    iam=false;
+    println!("{}",iam);
+
+    let mystr:char='😊';
+    println!("{}",mystr);
+
+    let mut name:&str="ayush";
+    name="singh";
+    println!("{}",name);
+
+    //in tuples u can have multiple data types
+    let name1:(&str,&str,i32)=("ayush","singh",40);
+   println!("{:?}",name1);
+
+   // if default formatter error  {} --> {:?}
+
+   let ages:[u16;7]=[40,30,50,60,70,80,90];
+    println!("{:?}",ages);
+
+    let new_ages:&[u16]=&ages[1..=4];
+    println!("{:?}",new_ages);   
+ 
+}
+
+
+
+
