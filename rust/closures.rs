61c6dda351d5a3cf83173830e53162c8a5c20bde
ayushsingh82 <ayushsinghmi711@gmail.com>
2024-01-28 14:49:23 +0530

    closures mutability

--- rust/closures.rs
+++ rust/closures.rs
@@ -1,3 +1,8 @@
+struct Person{
+    first_name:String,
+    last_name:String
+}
+
 pub fn test_closures(){
 
     let add =|x:i8,y:i8| println!("Returning some text {} {}",x,y);
@@ -13,6 +18,11 @@ pub fn test_closures(){
 
    let print_result=|| println!("result is {}",result);
    print_result();
+
+    let mut p1=Person{first_name:"Trevour".to_string(),last_name:"Sullivan".to_string()};
+    let mut  change_name=|new_lat_name:&str| p1.last_name=new_lat_name.to_string();
+    change_name("Johens");
+    println!("{}",p1.last_name);
     
     
 }
