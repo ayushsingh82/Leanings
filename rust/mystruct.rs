ff0a791834b636cfea64d9c8c2b7490ee58d41c8
ayushsingh82 <ayushsinghmi711@gmail.com>
2024-02-04 17:08:10 +0530

    impl struct

--- rust/mystruct.rs
+++ rust/mystruct.rs
@@ -23,16 +23,31 @@ struct Vehicle{
     color:VehicleColor,
 }
 
+impl Vehicle{
+     fn  paint(&mut self,new_color:VehicleColor){
+        self.color=new_color;
+     }
+
+     fn create_vehicle()->Vehicle{
+        let new_vehicle=Vehicle{manufacture:"default".to_string(),model:"default".to_string(),year:1990,color:VehicleColor::Blue};
+        return new_vehicle;
+     }
+}
+
  struct Person<'p>{
      first_name:Cell<&'p str>,
      last_name:String,
      birth_year:u16,
      birth_month:u8,
+     meters_walked:u32,
 }
 
 
+
 pub fn create_vehicle(){
-    let myvehicle=new_vehicle();
+    // let myvehicle=new_vehicle();
+    let mut myvehicle=Vehicle::create_vehicle();
+    myvehicle.paint(VehicleColor::Red);
     println!("{:?}",myvehicle)
 }
 
@@ -47,13 +62,14 @@ fn new_vehicletuple()->VehicleTuple{
 }
 
 fn new_vehicle()->Vehicle{
-    let v1=Vehicle{manufacture:"Porsche".to_string(),model:"Cyane".to_string(),year:1993,color:VehicleColor::Red};
+    let mut v1=Vehicle{manufacture:"Porsche".to_string(),model:"Cyane".to_string(),year:1993,color:VehicleColor::Red};
+    v1.paint(VehicleColor::White);
     return v1;
 }
 
 fn new_person()->Person<'static>{
     let  p1=Person{first_name:Cell::from("Ayush"),last_name:"Singh".to_string(),
-birth_year:2003,birth_month:7};
+birth_year:2003,birth_month:7,meters_walked:0};
 p1.first_name.set("Shannon");
     return p1;
 }
@@ -61,5 +77,5 @@ p1.first_name.set("Shannon");
 pub fn test_create_person(){
     let myperson=new_person();
     println!("Fist name:{0} , last name:{1},birth month :{2},
-    birth_year:{3}",myperson.first_name.get(),myperson.last_name,myperson.birth_month,myperson.birth_year);
+    birth_year:{3},meters_walked:{4}",myperson.first_name.get(),myperson.last_name,myperson.birth_month,myperson.birth_year,myperson.meters_walked);
 }
