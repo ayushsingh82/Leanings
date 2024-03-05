a6ee9c799f131b59f506d0f53592e43465b43ead
ayushsingh82 <ayushsinghmi711@gmail.com>
2024-03-05 20:42:18 +0530

    time diff

--- rust/mydatetime.rs
+++ rust/mydatetime.rs
@@ -4,9 +4,18 @@ pub fn test_stdtime(){
      let dur1= Duration::from_secs(15);
      println!("{}",dur1.as_millis());
 
-     let dur2=Duration::from_millis(5500);
-     let dur3=dur1.sub(dur2);
+     let dur2=Duration::from_millis(55500);
+    //  let dur3=dur1.sub(dur2);
 
-     println!("{}",dur3.as_millis());
+    //  println!("{}",dur3.as_millis());
+
+     let dur4=dur1.checked_sub(dur2);
+     println!("{}",dur4.unwrap_or_default().as_millis());
+
+     let now=Instant::now();
+
+     std::thread::sleep(Duration::from_millis(200));
+
+     println!("Elapsed time is {}",now.elapsed().as_micros());
  
 }
